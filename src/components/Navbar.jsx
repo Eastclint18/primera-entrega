516c951c30dc288110c4a88929eeb8689ec9e34c
Eastclint18 <igorivas18@gmail.com>
2023-10-27 02:39:24 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+export const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="">Teclados</a>
+        </li>
+        <li>
+          <a href="">Mouses</a>
+        </li>
+        <li>
+          <a href="">Monitores</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
